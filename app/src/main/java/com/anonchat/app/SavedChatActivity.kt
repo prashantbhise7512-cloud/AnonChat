120afdc4aebafb59abb2d09849adebe3ca337c2b
Prashant Bhise <prashant.bhise@neeyamo.com>
2026-08-20 17:43:57 +0530

    Antigravity Fix

--- app/src/main/java/com/anonchat/app/SavedChatActivity.kt
+++ app/src/main/java/com/anonchat/app/SavedChatActivity.kt
@@ -3,6 +3,7 @@ package com.anonchat.app
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
